313733b775f554016066e11ce2ff087cb6e37fc0
Carlos Mario <cm.alba10@uniandes.edu.co>
2025-03-03 11:03:44 -0500

    updated interest calculator challenge

--- 3.4.1. Simple-Interest-Calculator-master/script.js
+++ 3.4.1. Simple-Interest-Calculator-master/script.js
@@ -1,6 +1,24 @@
-function compute()
+function computeInterest()
 {
-    p = document.getElementById("principal").value;
-    
+    var pricipal = document.getElementById("principal").value;
+    pricipal = parseInt(pricipal, 10);
+
+    var rate = document.getElementById("rate").value;
+    rate = parseFloat(rate);
+
+    var years  = document.getElementById("years").value;
+    years  = parseInt(years , 10);
+
+    var interest  = pricipal * years * rate / 100;
+
+    var amount = pricipal + parseInt(interest);
+
+    var result = document.getElementById("result");
 }
-        
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    
+    document.getElementById("rate_val").innerText = rateval;
+}
+
