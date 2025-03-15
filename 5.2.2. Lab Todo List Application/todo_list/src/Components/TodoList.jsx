c1210645dcc7be92051ebfa5d05e811807a62d1e
Carlos Mario <cm.alba10@uniandes.edu.co>
2025-03-15 08:21:53 -0500

    created ToDo list initial developments

---
+++ 5.2.2. Lab Todo List Application/todo_list/src/Components/TodoList.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './TodoList.css';
+
+const TodoList = () => {
+
+  const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+
+  const handleAddTodo = () => {
+    if (headingInput.trim() !== '') {
+      setTodos([...todos, { heading: headingInput, lists: [] }]);
+      setHeadingInput('');
+    }
+  };
+  
+  return (
+    <>
+      <div className="todo-container">
+        <h1 className="title">My Todo List</h1>
+        <div className="input-container">
+          <input
+            type="text"
+            className="heading-input"
+            placeholder="Enter heading"
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}} // Add onChange event handler to update headingInput state
+          />
+          <button className="add-list-button">Add Heading</button>
+        </div>
+      </div>
+      <div className="todo_main">
+        
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
